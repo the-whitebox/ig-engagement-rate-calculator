106f5180b16f68277528adb6f6f8383da16762fa
tahir-atta <tahir@whiteboxtech.net>
2023-03-24 11:00:28 +0500

    requirements file added

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/muhammadtahir/workspace/django_projects/ig-engagement-rate-calculator/venv/bin/python3.10
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
